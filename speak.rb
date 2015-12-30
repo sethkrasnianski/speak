f66cbe4c1259318c1c115b88e68b6f4c845fe902
Seth Krasnianski <seth@mojotech.com>
2015-12-30 13:59:38 -0500

    Add timer and speaking functionality
    Loop through each item in the speach file and map out pertinent data. A
    timer is set to run until the max time in the speech file is hit. If the
    current timer time equals a time in one of the speech items, the item's
    message is spoken.

--- speak.rb
+++ speak.rb
@@ -1,9 +1,30 @@
 require 'espeak'
+require 'yaml'
 
 include ESpeak
 
-# TODO
-# - loop through file while timer is running and speak if looped time equals time in loop item's index
+file = YAML.load_file('starcraft.yml')
+second = 0
 
-speech = Speech.new("works")
-speech.speak
+def convert_time_to_seconds(t)
+  hours, minutes, seconds = t.match(/(^.*)(:)(.*)(:)(.*)/i).captures.reject! {|n| n == ':'}.map {|n| n.to_i}
+  hours *= 3600
+  minutes *= 60
+
+  hours + minutes + seconds
+end
+
+items = file.map do |time, message|
+  {time: convert_time_to_seconds(time), message: Speech.new(message)}
+end
+
+until second > items.last[:time]
+  item = items.select {|i| i[:time] == second}.first
+
+  if item
+    item[:message].speak
+  end
+
+  sleep(1)
+  second += 1
+end
